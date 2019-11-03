d6cae1294e982f17f7ce7d1292127ec6b8200bac
paolopetta <paolo.petta00@gmail.com>
2019-11-03 18:24:46 +0100

    Finito esercizistringhe

--- eclipse-workspace/EserciziStringhe/src/Tester.java
+++ eclipse-workspace/EserciziStringhe/src/Tester.java
@@ -5,9 +5,30 @@ public class Tester {
 		
 		//test country
 		ComparableDataSet<Country> dataCountry= new ComparableDataSet<Country>();
+		dataCountry.add(new Country ("Italia", 1000));
+		dataCountry.add(new Country("Romania", 500));
+		dataCountry.add(new Country ("Belgio", 540));
 		
+		Country minCountry = dataCountry.getMin();
+		Country maxCountry = dataCountry.getMax();
 		
+		System.out.println("dataCountry");
+		System.out.println("Max= "+maxCountry);
+		System.out.println("Min= "+minCountry);
 		
+		//test clienti
+		ComparableDataSet<Cliente> dataCliente= new ComparableDataSet<Cliente>();
+		dataCliente.add(new Cliente (50, "Alessio"));
+		dataCliente.add(new Cliente(20, "Den"));
+		dataCliente.add(new Cliente (25, "Luigi"));
 		
+		Cliente minCliente= dataCliente.getMin();
+		Cliente maxCliente= dataCliente.getMax();
+		
+		System.out.println("dataCliente");
+		System.out.println("Max= "+maxCliente);
+		System.out.println("Min= "+minCliente);
+		
+				
 	}
 }
